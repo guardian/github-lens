ceb93b6dcae16a53315f9a18d76d45d591facb73
Natasha <67543397+NovemberTang@users.noreply.github.com>
2023-11-29 16:10:11 +0000

    create a temporary job to repopulate a couple of tables

--- packages/cdk/lib/cloudquery/index.ts
+++ packages/cdk/lib/cloudquery/index.ts
@@ -319,6 +319,28 @@ export function addCloudqueryEcsCluster(
 			additionalCommands: additionalGithubCommands,
 		},
 		{
+			name: 'GitHubTeamsSubset',
+			description: 'Temporary job to repopulate github_team_repositories',
+			schedule:
+				nonProdSchedule ??
+				Schedule.cron({ weekDay: '1', hour: '10', minute: '0' }),
+			config: githubSourceConfig({
+				tables: ['github_organizations', 'github_team_repositories'],
+				skipTables: [
+					/*
+          These tables are children of github_organizations.
+          ServiceCatalogue collects child tables automatically.
+          We don't use them as they take a long time to collect, so skip them.
+          See https://www.cloudquery.io/docs/advanced-topics/performance-tuning#improve-performance-by-skipping-relations
+           */
+					'github_organization_dependabot_alerts',
+					'github_organization_dependabot_secrets',
+				],
+			}),
+			secrets: githubSecrets,
+			additionalCommands: additionalGithubCommands,
+		},
+		{
 			name: 'GitHubIssues',
 			description: 'Collect GitHub issue data (PRs and Issues)',
 			schedule: nonProdSchedule ?? Schedule.cron({ minute: '0', hour: '2' }),
