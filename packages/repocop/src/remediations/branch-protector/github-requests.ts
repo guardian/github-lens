ebb0b23938224a12735e3a51a21d5b9d34d16dce
Natasha <67543397+NovemberTang@users.noreply.github.com>
2023-12-07 18:24:07 +0000

    use current protection to feed into new protection

--- packages/repocop/src/remediations/branch-protector/github-requests.ts
+++ packages/repocop/src/remediations/branch-protector/github-requests.ts
@@ -1,5 +1,66 @@
 import type { Octokit } from 'octokit';
-import type { UpdateBranchProtectionParams } from './model';
+import type {
+	CurrentBranchProtection,
+	UpdateBranchProtectionParams,
+} from './model';
+
+async function getCurrentBranchProtection(
+	octokit: Octokit,
+	owner: string,
+	repo: string,
+	branch: string,
+): Promise<CurrentBranchProtection> {
+	const branchProtectionParams = {
+		owner: owner,
+		repo: repo,
+		branch: branch,
+	};
+	const branchProtection = await octokit.rest.repos.getBranchProtection(
+		branchProtectionParams,
+	);
+
+	return branchProtection.data;
+}
+
+//TODO test this
+function constructNewBranchProtection(
+	protection: CurrentBranchProtection,
+	owner: string,
+	repo: string,
+	branch: string,
+): UpdateBranchProtectionParams {
+	const required_approving_review_count =
+		protection.required_pull_request_reviews?.required_approving_review_count ??
+		1;
+	const users =
+		protection.restrictions?.users.map((user) => user.login as string) ?? [];
+	const teams =
+		protection.restrictions?.teams.map((team) => team.slug as string) ?? [];
+	const apps =
+		protection.restrictions?.apps.map((app) => app.slug as string) ?? [];
+	const newProtection: UpdateBranchProtectionParams = {
+		owner: owner,
+		repo: repo,
+		branch: branch,
+		required_status_checks: {
+			strict: true,
+			contexts: protection.required_status_checks?.contexts ?? [],
+		},
+		restrictions: {
+			users,
+			teams,
+			apps,
+		},
+		enforce_admins: true,
+		required_pull_request_reviews: {
+			require_code_owner_reviews: true,
+			required_approving_review_count,
+		},
+		allow_force_pushes: protection.allow_force_pushes?.enabled,
+		allow_deletions: false,
+	};
+	return newProtection;
+}
 
 export async function updateBranchProtection(
 	octokit: Octokit,
@@ -7,27 +68,23 @@ export async function updateBranchProtection(
 	repo: string,
 	branch: string,
 ) {
+	const protection = await getCurrentBranchProtection(
+		octokit,
+		owner,
+		repo,
+		branch,
+	);
+
 	console.log(`Applying branch protection to ${repo}`);
-	//https://github.com/guardian/recommendations/blob/main/github.md#branch-protection
-	const branchProtectionParams: UpdateBranchProtectionParams = {
-		owner: owner,
-		repo: repo,
-		branch: branch,
-		required_status_checks: {
-			strict: true,
-			contexts: [],
-		},
-		restrictions: null,
-		enforce_admins: true,
-		required_pull_request_reviews: {
-			require_code_owner_reviews: true,
-			required_approving_review_count: 1,
-		},
-		allow_force_pushes: false,
-		allow_deletions: false,
-	};
+
+	const newProtection = constructNewBranchProtection(
+		protection,
+		owner,
+		repo,
+		branch,
+	);
 	try {
-		await octokit.rest.repos.updateBranchProtection(branchProtectionParams);
+		await octokit.rest.repos.updateBranchProtection(newProtection);
 	} catch (error) {
 		console.error(`Error: branch protection failed for ${repo}`);
 		console.error(error);
--- packages/repocop/src/remediations/branch-protector/model.ts
+++ packages/repocop/src/remediations/branch-protector/model.ts
@@ -1,4 +1,12 @@
+import type { components } from '@octokit/openapi-types';
+import type { RestEndpointMethodTypes } from '@octokit/plugin-rest-endpoint-methods/dist-types/generated/parameters-and-response-types';
 import type { Endpoints } from '@octokit/types';
 
 export type UpdateBranchProtectionParams =
 	Endpoints['PUT /repos/{owner}/{repo}/branches/{branch}/protection']['parameters'];
+
+export type GetBranchProtectionParams =
+	RestEndpointMethodTypes['repos']['getBranchProtection']['response'];
+
+export type CurrentBranchProtection =
+	components['schemas']['branch-protection'];
