392df712d1c95c9aaa2510c748e92820a87cf34f
Chris Jones <chrislomaxjones96@gmail.com>
2024-03-14 12:55:12 +0000

    Stack and region as consts

--- packages/cdk/bin/cdk.ts
+++ packages/cdk/bin/cdk.ts
@@ -1,22 +1,25 @@
 import 'source-map-support/register';
-import { GuRoot } from '@guardian/cdk/lib/constructs/root';
-import { Duration } from 'aws-cdk-lib';
+import { RiffRaffYamlFile } from '@guardian/cdk/lib/riff-raff-yaml-file';
+import { App, Duration } from 'aws-cdk-lib';
 import { Schedule } from 'aws-cdk-lib/aws-events';
 import { ServiceCatalogue } from '../lib/service-catalogue';
 
-const app = new GuRoot();
+const app = new App();
+
+const stack = 'deploy';
+const region = 'eu-west-1';
 
 new ServiceCatalogue(app, 'ServiceCatalogue-PROD', {
-	stack: 'deploy',
+	stack,
 	stage: 'PROD',
-	env: { region: 'eu-west-1' },
+	env: { region },
 	cloudFormationStackName: 'deploy-PROD-service-catalogue',
 });
 
 new ServiceCatalogue(app, 'ServiceCatalogue-CODE', {
-	stack: 'deploy',
+	stack,
 	stage: 'CODE',
-	env: { region: 'eu-west-1' },
+	env: { region },
 	schedule: Schedule.rate(Duration.days(30)),
 	rdsDeletionProtection: false,
 	cloudFormationStackName: 'deploy-CODE-service-catalogue',
