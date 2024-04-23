f6e4eb5cc82c57ed08e366b00c667667156604b0
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-04-23 17:13:03 +0100

    highlight only new(-ish) findings

--- packages/cloudbuster/src/index.ts
+++ packages/cloudbuster/src/index.ts
@@ -1,14 +1,33 @@
-import type { aws_securityhub_findings, PrismaClient } from '@prisma/client';
+import type { PrismaClient } from '@prisma/client';
 import { getPrismaClient } from 'common/database';
+import { partition } from 'common/src/functions';
 import { config } from 'dotenv';
 import { getConfig } from './config';
+import type { Finding } from './types';
 
 config({ path: `../../.env` }); // Load `.env` file at the root of the repository
 
 export async function main() {
 	const config = await getConfig();
 	const prisma: PrismaClient = getPrismaClient(config);
-	const findings: aws_securityhub_findings[] =
-		await prisma.aws_securityhub_findings.findMany();
-	console.log(findings.slice(0, 5));
+	const findings: Finding[] = (
+		await prisma.aws_securityhub_findings.findMany()
+	).map((f) => f as unknown as Finding);
+
+	const [criticals, highs] = partition(
+		findings,
+		(f) => f.severity.Label === 'CRITICAL',
+	);
+
+	const twoDaysAgo = new Date();
+	twoDaysAgo.setDate(twoDaysAgo.getDate() - 2);
+
+	const inDateCriticals = criticals.filter((f) => f.created_at > twoDaysAgo);
+	console.log(inDateCriticals.map((f) => f.title));
+
+	const twoWeeksAgo = new Date();
+	twoWeeksAgo.setDate(twoWeeksAgo.getDate() - 14);
+
+	const inDateHighs = highs.filter((f) => f.created_at > twoWeeksAgo);
+	console.log(inDateHighs.map((f) => f.title));
 }
