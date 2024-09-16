0f34ce845d1f1f9ad651107026a401bf34473b46
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-09-16 19:40:01 +0100

    only send cloudbuster notifications for events less than 60 days old

--- packages/cloudbuster/src/index.ts
+++ packages/cloudbuster/src/index.ts
@@ -5,6 +5,7 @@ import type { SecurityHubSeverity } from 'common/types';
 import { getConfig } from './config';
 import { createDigestsFromFindings, sendDigest } from './digests';
 import { transformFinding } from './findings';
+import type { Digest, Finding } from './types';
 
 type LambdaHandlerProps = {
 	severities?: SecurityHubSeverity[];
@@ -25,10 +26,13 @@ export async function main(input: LambdaHandlerProps) {
 		`Starting Cloudbuster. Level of severities that will be scanned: ${severities.join(', ')}`,
 	);
 
-	const findings = (await getFsbpFindings(prisma, severities)).map((f) =>
-		transformFinding(f),
-	);
-	const digests = createDigestsFromFindings(findings);
+	const sixtyDaysAgo = new Date();
+	sixtyDaysAgo.setDate(sixtyDaysAgo.getDate() - 60);
+
+	const findings: Finding[] = (await getFsbpFindings(prisma, severities))
+		.filter((f) => f.first_observed_at && f.first_observed_at > sixtyDaysAgo)
+		.map((f) => transformFinding(f));
+	const digests: Digest[] = createDigestsFromFindings(findings);
 
 	// *** NOTIFICATION SENDING ***
 	const anghammaradClient = new Anghammarad();
